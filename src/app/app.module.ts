01d0286119eb094343e4068e6cc8f20bd33c927c
Mariana <marianafloriano24@gmail.com>
2025-02-28 15:26:20 -0300

    atividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+          num1: number = 0;
+          num2: number = 0;
+          sun : number = 0;
 
+          onClickSomar(){
+            this.sun = this.num1 + this.num2;
+          }
+          onClickSubtrair(){
+            this.sun = this.num1 - this.num2;
+          }
+          onClickDividir(){
+            this.sun = this.num1 / this.num2;
+          }
+          onClickMult(){
+            this.sun = this.num1 * this.num2;
+          }
+          onClickPoten(){
+            this.sun = this.num1 ** this.num2;
+          }
+          onClickMod(){
+            this.sun = this.num1 % this.num2;
+          }
 }
